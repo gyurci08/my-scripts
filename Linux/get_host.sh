7ae58174a5f5d0f1a591958f7b171be4be8e676c
gyurci08 <jandzso.gyorgy@webstar.hu>
2025-04-01 14:58:54 +0200

    works

--- Linux/get_host.sh
+++ Linux/get_host.sh
@@ -14,17 +14,50 @@ if [ -z "$1" ]; then
 fi
 
 awk -v pattern="$1" '
-    {sub(/^ +/, "")} # Remove leading whitespace
-    $1 == "Host" { host = $2; hostname = "" }
-    $1 == "Hostname" { hostname = $2 }
-    $0 == "" || $1 == "Host " {
-        if (hostname != "" && hostname !~ /\*/ && hostname ~ pattern && !seen[hostname]) {
-            print hostname
-            seen[hostname] = 1
-        } else if (hostname == "" && host !~ /\*/ && host ~ pattern && !seen[host]) {
-            print host
-            seen[host] = 1
+    # Convert pattern to lowercase once
+    BEGIN { lp = tolower(pattern) }
+
+    # Remove leading whitespace and skip empty lines
+    {sub(/^ +/, ""); if (NF == 0) next}
+
+    # Handle Host lines
+    /^Host/ {
+        if (NF == 2) {                             # Only one Host
+            single_host = $2
+            lc_str = tolower(single_host)
+            if (lc_str !~ /\*/ && lc_str ~ lp) {
+                if (!seen[single_host]) {
+                    known_host = single_host
+                }
+            }
+        } else { # Multiple Hosts
+            for (i = 2; i <= NF; i++) {
+                lc_str = tolower($i)
+                if (lc_str !~ /\*/ && lc_str ~ lp && !seen[$i]) {
+                    print $i
+                    seen[$i] = 1
+                }
+            }
+        }
+    }
+
+    # Handle Hostname lines
+    /^Hostname/ {
+        hostname = $2
+        if (single_host != "") {                   # If there was only one Host
+            lc_str = tolower(hostname)
+            if (lc_str !~ /\*/ && lc_str ~ lp && !seen[hostname]) {
+                print hostname
+                seen[hostname] = 1
+                known_host = ""
+            }
+        }
+    }
+
+    END {
+        if (known_host != "" && !seen[known_host]) {
+            print known_host
+            seen[known_host] = 1
         }
     }
 ' ~/.ssh/config
-
--- Linux/xssh.sh
+++ Linux/xssh.sh
@@ -15,19 +15,54 @@ fi
 
 # Get the hosts from the SSH config
 HOSTS=$(awk -v pattern="$1" '
-    {sub(/^ +/, "")} # Remove leading whitespace
-    $1 == "Host" { host = $2; hostname = "" }
-    $1 == "Hostname" { hostname = $2 }
-    $0 == "" || $1 == "Host " {
-        if (hostname != "" && hostname !~ /\*/ && hostname ~ pattern && !seen[hostname]) {
-            print hostname
-            seen[hostname] = 1
-        } else if (hostname == "" && host !~ /\*/ && host ~ pattern && !seen[host]) {
-            print host
-            seen[host] = 1
+    # Convert pattern to lowercase once
+    BEGIN { lp = tolower(pattern) }
+
+    # Remove leading whitespace and skip empty lines
+    {sub(/^ +/, ""); if (NF == 0) next}
+
+    # Handle Host lines
+    /^Host/ {
+        if (NF == 2) {                             # Only one Host
+            single_host = $2
+            lc_str = tolower(single_host)
+            if (lc_str !~ /\*/ && lc_str ~ lp) {
+                if (!seen[single_host]) {
+                    known_host = single_host
+                }
+            }
+        } else { # Multiple Hosts
+            for (i = 2; i <= NF; i++) {
+                lc_str = tolower($i)
+                if (lc_str !~ /\*/ && lc_str ~ lp && !seen[$i]) {
+                    print $i
+                    seen[$i] = 1
+                }
+            }
         }
     }
-' ~/.ssh/config)
+
+    # Handle Hostname lines
+    /^Hostname/ {
+        hostname = $2
+        if (single_host != "") {                   # If there was only one Host
+            lc_str = tolower(hostname)
+            if (lc_str !~ /\*/ && lc_str ~ lp && !seen[hostname]) {
+                print hostname
+                seen[hostname] = 1
+                known_host = ""
+            }
+        }
+    }
+
+    END {
+        if (known_host != "" && !seen[known_host]) {
+            print known_host
+            seen[known_host] = 1
+        }
+    }
+' ~/.ssh/config
+)
 
 # Check if hosts were found
 if [ -z "$HOSTS" ]; then
@@ -48,7 +83,10 @@ fi
 # Check if multiple hosts are found and prompt for confirmation
 if [ $(echo "$HOSTS" | wc -w) -gt 1 ]; then
     echo "Multiple hosts found:"
-    echo "$HOSTS"
+    echo "The following hosts will be affected:"
+    for HOST in $HOSTS; do
+        echo "- $HOST"
+    done
     read -p "Are you sure you want to execute the command on all these hosts? (y/n): " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Nn]$ ]]; then
