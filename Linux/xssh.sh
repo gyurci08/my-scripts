92f9760d88cc4859857fc42c3095d3c22c2527de
gyurci08 <jandzso.gyorgy@webstar.hu>
2025-04-01 16:04:23 +0200

    works

--- Linux/xssh.sh
+++ Linux/xssh.sh
@@ -25,54 +25,48 @@ fi
 
 # Get the hosts from the SSH config
 HOSTS=$(awk -v pattern="$HOST_PATTERN" '
-    # Convert pattern to lowercase once
-    BEGIN { lp = tolower(pattern) }
+    # Remove leading/trailing whitespace
+    { sub(/^ +/, ""); sub(/ +$/, ""); }
 
-    # Remove leading whitespace and skip empty lines
-    {sub(/^ +/, ""); if (NF == 0) next}
+    # Skip empty lines or comments
+    /^$/ || /^#/ { next }
 
-    # Handle Host lines
-    /^Host/ {
-        if (NF == 2) {                             # Only one Host
-            single_host = $2
-            lc_str = tolower(single_host)
-            if (lc_str !~ /\*/ && lc_str ~ lp) {
-                if (!seen[single_host]) {
-                    known_host = single_host
-                }
-            }
-        } else { # Multiple Hosts
-            for (i = 2; i <= NF; i++) {
-                lc_str = tolower($i)
-                if (lc_str !~ /\*/ && lc_str ~ lp && !seen[$i]) {
-                    print $i
-                    seen[$i] = 1
-                }
-            }
+    # Handle Hostname lines and associate them with the current host
+    /^Hostname/ {
+        if (current_host) {
+            hostnames[current_host] = $2
         }
     }
 
-    # Handle Hostname lines
-    /^Hostname/ {
-        hostname = $2
-        if (single_host != "") {                   # If there was only one Host
-            lc_str = tolower(hostname)
-            if (lc_str !~ /\*/ && lc_str ~ lp && !seen[hostname]) {
-                print hostname
-                seen[hostname] = 1
-                known_host = ""
+    # Handle Host lines
+    /^Host/ {
+        split($0, hosts, " ")
+        for (i=2; i<=NF; i++) {
+            if (hosts[i] !~ /\*/) {
+                current_host = hosts[i]
+                hosts_array[hosts[i]] # Store host without *
             }
         }
     }
 
     END {
-        if (known_host != "" && !seen[known_host]) {
-            print known_host
-            seen[known_host] = 1
+        for (host in hosts_array) {
+            if (tolower(host) ~ pattern && host !~ /\*$/) {
+                if (hostnames[host]) {
+                    if (!(hostnames[host] in printed)) {
+                        print hostnames[host] # Print Hostname if available
+                        printed[hostnames[host]] = 1
+                    }
+                } else {
+                    if (!(host in printed)) {
+                        print host # Otherwise, print Host entry itself
+                        printed[host] = 1
+                    }
+                }
+            }
         }
     }
-' ~/.ssh/config
-)
+' ~/.ssh/config)
 
 # Check if hosts were found
 if [ -z "$HOSTS" ]; then
